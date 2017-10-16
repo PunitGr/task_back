29b9f4512ae5358f0fa141b99d13940523389596
PunitGr <groove679@gmail.com>
2017-10-17 04:55:27 +0530

    Add Member model

--- team/models.py
+++ team/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+role_choices = (
+    ('R', 'Regular'),
+    ('A', 'Admin')
+)
+
+
+class Member(models.Model):
+    userId = models.IntegerField(blank=False, primary_key=True)
+    firstName = models.CharField(blank=False, max_length=40)
+    lastName = models.CharField(blank=True, null=True, max_length=40)
+    phoneNumber = PhoneNumberField()
+    email = models.EmailField(max_length=240, blank=False, unique=True)
+    role = models.CharField(max_length=7, default='R', choices=role_choices)
