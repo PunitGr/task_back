5df29c8a98e51fa797b18446aac0eb0fd7e5385d
PunitGr <groove679@gmail.com>
2017-10-18 04:17:49 +0530

    Update test cases

--- team/tests.py
+++ team/tests.py
@@ -1,12 +1,7 @@
-import urllib
 from django.core.urlresolvers import reverse
 
 from rest_framework.test import APITestCase
-from .models import Member
-
-
-def reverse_with_urlparams(path, **kwargs):
-    return path + '?' + urllib.parse.urlencode(kwargs)
+from rest_framework import status
 
 
 class MemberTestCase(APITestCase):
@@ -14,72 +9,77 @@ class MemberTestCase(APITestCase):
     Test Case to check MemberView
     """
 
-    def create_member(self, userId, email):
+    def create_member(self, email):
         url = reverse("users")
         data = {
-            "userId": userId,
             "email": email,
             "firstName": "Test",
             "lastName": "Last",
             "phoneNumber": "+919911139678",
-            "role": 0
+            "role": "R"
         }
 
         return self.client.post(url, data, headers={"Content-Type": "application/json"})
 
     def test_post(self):
-        response = self.create_member(1, "test@example.com")
-        self.assertEqual(201, response.status_code)
+        response = self.create_member("test@example.com")
+        self.assertTrue(status.is_success(response.status_code))
 
     def test_get_no_members(self):
         url = reverse("users")
         response = self.client.get(url, headers={"Content-Type": "application/json"})
-        self.assertEqual(404, response.status_code)
+        self.assertTrue(status.is_success(response.status_code))
 
-    def test_get(self):
-        self.create_member(2, "test123@example.com")
+    def test_get_all_members(self):
+        self.create_member("test123@example.com")
         url = reverse("users")
 
         # To check all the team members
         response = self.client.get(url, headers={"Content-Type": "application/json"})
-        self.assertEqual(200, response.status_code)
-
-        # check a specific team member
-        url = reverse_with_urlparams(reverse("users"), user=2)
-        self.assertEqual(200, response.status_code)
-
-    def test_delete(self):
-        self.create_member(3, "test1234@example.com")
-        url = reverse_with_urlparams(reverse("users"), user=3)
-
-        # To check all the team members
-        response = self.client.delete(url, headers={"Content-Type": "application/json"})
-        self.assertEqual(200, response.status_code)
+        self.assertTrue(status.is_success(response.status_code))
 
 
 class EditMemberTestCase(APITestCase):
     """
     Test Case for Editting member details
     """
-    def create_member(self, userId, email):
+    def create_member(self, email):
         url = reverse("users")
         data = {
-            "userId": userId,
             "email": email,
             "firstName": "Test",
             "lastName": "Last",
             "phoneNumber": "+919911139678",
-            "role": 0
+            "role": "R"
         }
 
-        self.client.post(url, data, headers={"Content-Type": "application/json"})
+        response = self.client.post(url, data, headers={"Content-Type": "application/json"})
+        return response.data
+
+    def test_get_member(self):
+        data = self.create_member("test112345@example.com")
+        id = data["id"]
+
+        url = reverse("edit", kwargs={"pk": id})
+        response = self.client.get(url, headers={"Content-Type": "application/json"})
+        self.assertTrue(status.is_success(response.status_code))
 
     def test_patch(self):
-        self.create_member(4, "test12345@example.com")
-        url = reverse("edit", kwargs={"user_id": 4})
+        data = self.create_member("test12345@example.com")
+        id = data["id"]
+
+        url = reverse("edit", kwargs={"pk": id})
         data = {
             "firstName": "Tester"
         }
         response = self.client.patch(url, data, headers={"Content-Type": "application/json"})
-        self.assertEqual(201, response.status_code)
-        self.assertEqual("Successfully updated info", response.data["results"])
+        self.assertTrue(status.is_success(response.status_code))
+
+    def test_delete(self):
+        data = self.create_member("test1234@example.com")
+        id = data["id"]
+        url = reverse("edit", kwargs={"pk": id})
+
+        # To check all the team members
+        response = self.client.delete(url, headers={"Content-Type": "application/json"})
+        self.assertTrue(status.is_success(response.status_code))
