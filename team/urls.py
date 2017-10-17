2895a88b192d981a4c5785a3092cc524388dae5a
PunitGr <groove679@gmail.com>
2017-10-18 03:54:59 +0530

    Update views to generic views

--- team/urls.py
+++ team/urls.py
@@ -3,5 +3,5 @@ from .views import MemberView, EditMemberView
 
 urlpatterns = [
     url(r'^$', MemberView.as_view(), name="users"),
-    url(r'^edit/(?P<user_id>[0-9]+)/$', EditMemberView().as_view(), name="edit")
+    url(r'^(?P<pk>[0-9]+)/$', EditMemberView().as_view(), name="edit")
 ]
